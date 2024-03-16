65cf2b0dff4c7a75e256af48a8a9d2f0fcaa997b
milenaleonardi <milena.leonardi@hotmail.com>
2024-03-16 09:33:17 -0300

    game v1.0.0

---
+++ app.js
@@ -0,0 +1,92 @@
+let saldo = 100;
+let winningCarId = null;
+let posX1 = 0,
+  posX2 = 0,
+  posX3 = 0,
+  posX4 = 0,
+  posX5 = 0;
+let winner = null;
+let myInterval;
+const btn = document.getElementById("btn-start");
+btn.addEventListener("click", startRace);
+
+function moveCar() {
+  if (posX1 > 700 || posX2 > 700 || posX3 > 700 || posX4 > 700 || posX5 > 700) {
+    if (posX1 > 700 && !winner) {
+      winner = "pilot 1";
+      winningCarId = "1";
+    } else if (posX2 > 700 && !winner) {
+      winner = "pilot 2";
+      winningCarId = "2";
+    } else if (posX3 > 700 && !winner) {
+      winner = "pilot 3";
+      winningCarId = "3";
+    } else if (posX4 > 700 && !winner) {
+      winner = "pilot 4";
+      winningCarId = "4";
+    } else if (posX5 > 700 && !winner) {
+      winner = "pilot 5";
+      winningCarId = "5";
+    }
+
+    myStop(winner);
+  } else {
+    posX1 += Math.floor(Math.random() * 5);
+    const div1 = document.getElementById("car1");
+    div1.style.transform = "translate(" + posX1 + "px)";
+
+    posX2 += Math.floor(Math.random() * 5);
+    const div2 = document.getElementById("car2");
+    div2.style.transform = "translate(" + posX2 + "px)";
+
+    posX3 += Math.floor(Math.random() * 5);
+    const div3 = document.getElementById("car3");
+    div3.style.transform = "translate(" + posX3 + "px)";
+
+    posX4 += Math.floor(Math.random() * 5);
+    const div4 = document.getElementById("car4");
+    div4.style.transform = "translate(" + posX4 + "px)";
+
+    posX5 += Math.floor(Math.random() * 5);
+    const div5 = document.getElementById("car5");
+    div5.style.transform = "translate(" + posX5 + "px)";
+  }
+}
+
+function startRace() {
+  document.getElementById("message").textContent = "";
+  winner = null;
+  const bet = document.getElementById("bet").value;
+  if (bet < 5) {
+    alert("A aposta mínima é de R$5.");
+    return;
+  }
+
+  if (saldo < bet) {
+    alert("Saldo insuficiente para fazer esta aposta.");
+    return;
+  }
+
+  (posX1 = 0), (posX2 = 0), (posX3 = 0), (posX4 = 0), (posX5 = 0);
+
+  myInterval = setInterval(moveCar, 50);
+}
+
+function myStop(winner) {
+  clearInterval(myInterval);
+
+  const chosenRacer = document.getElementById("option").value;
+  const bet = document.getElementById("bet").value;
+  let message;
+
+  if (chosenRacer === winningCarId) {
+    saldo += 2 * parseInt(bet);
+    const profit = 2 * parseInt(bet);
+    message = "Congratulations, you have won the race! You earned R$ " + profit;
+  } else {
+    saldo -= parseInt(bet);
+    message = "Sorry, you have lost. The winner is " + winner;
+  }
+  document.getElementById("value").textContent = saldo;
+  document.getElementById("message").textContent = message;
+}
